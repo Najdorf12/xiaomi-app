41c1ff50df2140c1165b09ccd9dbbf2bd0fabb12
Najdorf12 <agustin.morro@gmail.com>
2024-11-26 13:34:05 -0300

    add styles

--- src/components/Layout/Home.jsx
+++ src/components/Layout/Home.jsx
@@ -14,17 +14,17 @@ const Home = () => {
         <figure className="z-50 w-9">
           <Logo />
         </figure>
-        <ul className="flex justify-center font-medium items-center gap-6 font-title text-stone-300 text-sm xl:text-base xl:gap-9 2xl:text-xl ">
-          <li className="border-l-[1px] border-primary py-[1px] pl-2 xl:pl-3 ">
+        <ul className="flex justify-center font-medium items-center gap-6 font-title text-zinc-300 text-sm xl:text-base xl:gap-9 2xl:text-xl ">
+          <li className="border-l-[1px] border-zinc-300 py-[1px] pl-2 xl:pl-3 ">
             Servicio
           </li>
-          <li className="border-l-[1px] border-primary py-[1px] pl-2 xl:pl-3">
+          <li className="border-l-[1px] border-zinc-300 py-[1px] pl-2 xl:pl-3">
             Contacto
           </li>
         </ul>
       </nav>
 
-      <figure className="w-full h-full absolute inset-0 grayscale-[1]">
+      <figure className="w-full h-full absolute inset-0">
         <img
           src={bgHome}
           alt="bgHome"
@@ -38,28 +38,28 @@ const Home = () => {
         <h3 className="z-50 text-primary font-title font-semibold text-xl leading-[3rem]  xl:text-4xl xl:leading-[5rem]">
           SERVICIO TECNICO
           <br />
-          <span className="text-[4.7rem] font-medium text-white xl:text-[8.5rem] 2xl:text-[8.5rem]">
+          <span className="text-[4.7rem] font-medium text-zinc-200 xl:text-[8.5rem] 2xl:text-[8.5rem]">
             XIAOMI
           </span>
         </h3>
-        <p className="z-50 font-title mt-2 font-medium text-base text-zinc-600 lg:text-lg xl:mt-1 xl:text-xl  2xl:text-2xl  ">
+        <p className="z-50 font-title mt-2 font-medium text-base text-zinc-500 lg:text-lg xl:mt-1 xl:text-xl  2xl:text-2xl  ">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius placeat
           sapiente fuga eaque reprehenderit officia.
         </p>
         <div className="w-full font-text2 text-sm flex justify-start items-start mt-5 gap-3  lg:gap-12 lg:mt-4 xl:mt-6 2xl:mt-8">
           <button
             id="box-glass2"
-            className="group bg-gradient-to-l from-primary via-gray to-transparent text-stone-200 group border-[2px] rounded-3xl font-semibold  border-stone-500 pl-4 pr-1 py-[1px]  flex justify-between items-center min-w-[170px] md:text-base xl:py-[2px]  lg:border-stone-400  lg:w-[195px]  duration-500"
+            className="group bg-gradient-to-l from-primary via-gray to-transparent text-stone-200 group border-[2px] rounded-3xl font-semibold  border-zinc-500 pl-4 pr-1 py-[1px]  flex justify-between items-center min-w-[170px] md:text-base xl:py-[2px]  lg:border-zinc-400  lg:w-[195px]  duration-500"
           >
             SERVICIO
-            <i class="bx bx-arrow-back rotate-[145deg] text-2xl h-7 w-7 md:h-8 md:w-8 rounded-full bg-stone-500 text-stone-100 flex justify-center items-center  2xl:text-3xl group-hover:rotate-[220deg]  duration-500 font-normal"></i>
+            <i class="bx bx-arrow-back rotate-[145deg] text-2xl h-7 w-7 md:h-8 md:w-8 rounded-full bg-zinc-500 text-zinc-100 flex justify-center items-center  2xl:text-3xl group-hover:rotate-[220deg]  duration-500 font-normal"></i>
           </button>
           <button
             id="box-glass2"
-            className="group  text-stone-400 group border-[2px] rounded-3xl font-semibold  border-stone-500 pl-4 pr-1 py-[1px]  flex justify-between items-center min-w-[170px] md:text-base xl:py-[2px]  lg:border-stone-400 lg:text-stone-200  lg:w-[195px]  duration-500"
+            className="group  text-zinc-400 group border-[2px] rounded-3xl font-semibold  border-zinc-500 pl-4 pr-1 py-[1px]  flex justify-between items-center min-w-[170px] md:text-base xl:py-[2px]  lg:border-zinc-400 lg:text-zinc-200  lg:w-[195px]  duration-500"
           >
             CONTACTO
-            <i class="bx bx-arrow-back rotate-[145deg] text-2xl h-7 w-7 md:h-8 md:w-8 rounded-full bg-stone-500 text-stone-100 flex justify-center items-center  2xl:text-3xl group-hover:rotate-[220deg]  duration-500 font-normal"></i>
+            <i class="bx bx-arrow-back rotate-[145deg] text-2xl h-7 w-7 md:h-8 md:w-8 rounded-full bg-zinc-500 text-zinc-100 flex justify-center items-center  2xl:text-3xl group-hover:rotate-[220deg]  duration-500 font-normal"></i>
           </button>
         </div>
       </article>
