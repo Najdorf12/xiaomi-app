b3965984a9af73f5b5b455a1cc66eca147dd7502
Najdorf <agustin.morro@gmail.com>
2024-12-10 20:33:40 -0300

    add styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 
 const Card = ({cardData}) => {
   return (
-    <div id="box-glass2" className="w-[90%] h-[210px]  max-w-[400px]  bg-gradient-to-br from-gray via- to- pb-3 py-2 pl-3 pr-1 rounded-xl border border-stone-300 flex flex-col justify-between lg:pt-4 lg:pl-4 lg:pb-4 lg:h-[250px] lg:max-w-[390px] xl:h-[270px] shadow-lg shadow-gray  hover:border-primary duration-500 group 2xl:max-w-[430px] 2xl:h-[270px]  ">
+    <div id="box-glass2" className="w-[90%] h-[210px]  max-w-[400px]  bg-gradient-to-br from-gray via- to- pb-3 py-2 pl-3 pr-1 rounded-xl border border-stone-300 flex flex-col justify-between lg:pt-4 lg:pl-4 lg:pb-4 lg:h-[250px] lg:max-w-[390px] xl:h-[270px] shadow-lg shadow-gray  hover:border-primary duration-500 group 2xl:max-w-[430px] 2xl:h-[270px] ">
       <figure className="w-24 lg:w-28 group-hover:scale-105  duration-500">
         <img src={cardData?.icon} alt="icon" />
       </figure>
