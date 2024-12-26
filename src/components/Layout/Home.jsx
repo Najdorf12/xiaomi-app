cc098d34b7dc322838bd684569213e7c7509e376
Najdorf <agustin.morro@gmail.com>
2024-12-26 11:25:31 -0300

    add styles

--- src/components/Layout/Home.jsx
+++ src/components/Layout/Home.jsx
@@ -1,4 +1,4 @@
-import bgHomeMobile from "/st4.jpg";
+import bgHomeMobile from "/st10.png";
 import bgHomeDesktop from "/st8.png";
 import GradientBtn from "../Buttons/GradientBtn";
 import Logo from "../Logo";
