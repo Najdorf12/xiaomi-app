4560abb02a13ebd442a48bb06ccdc950298466b4
Najdorf <agustin.morro@gmail.com>
2025-01-06 18:27:56 -0300

    Add styles2

--- src/components/CardService.jsx
+++ src/components/CardService.jsx
@@ -4,14 +4,14 @@ const CardService = ({ serviceData }) => {
       id=""
       className="w-[100%] border-b-[2px] pb-3 px-6 border-orange-400  flex flex-col max-w-[500px] xl:pb-4 xl:max-w-[550px]"
     >
-      <figure className="w-20 self-end">
+      <figure className="w-20 self-end lg:w-16 xl:w-20">
         <img
           src={serviceData?.icon}
           alt="icon"
           className="object-cover object-center w-full h-full"
         />
       </figure>
-      <p className="text-stone-200 text-balance  font-medium text-sm font-text2 mt-2   lg:text-lg  lg:-mt-7 xl:pr-6 ">
+      <p className="text-stone-200 text-balance  font-medium text-sm font-text2 mt-2 lg:-mt-1  lg:text-lg xl:-mt-7 xl:pr-6 ">
       {serviceData?.content}
       </p>
     </li>
--- src/components/Layout/Home.jsx
+++ src/components/Layout/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
           </a>
         </ul>
       </nav>
-      <div className="lg:w-[40%] lg:self-start lg:mt-24 ">
+      <div className="lg:w-[45%] xl:w-[40%] lg:self-start lg:mt-24 ">
         <article className="relative mx-2 mt-6 text-start z-50 rounded-xl flex flex-col justify-start items-start  pt-2 pb-4 pl-3 text-balance  lg:text-start lg:justify-start lg:items-start  lg:mt-0 lg:pl-6 xl:pt-4 xl:pb-6 lg:w-[500px]">
           <h3 className="z-50 text-primary font-title font-semibold text-2xl leading-[3.3rem] lg:text-3xl lg:leading-[3.7rem]  xl:text-4xl xl:leading-[5rem] 2xl:text-5xl">
             SERVICIO TECNICO
